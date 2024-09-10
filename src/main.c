5869c0d69257e9e26c22938a2af6e7677f194257
Rafael Nascimento Cardoso <rafnasci@fu-r7-p1.s19.be>
2024-09-10 16:25:45 +0200

    ajout de la fonction pour afficher l'ast et utilisation de l'ast dans le main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 19:16:16 by rafnasci          #+#    #+#             */
-/*   Updated: 2024/09/09 16:22:17 by rafnasci         ###   ########.fr       */
+/*   Updated: 2024/09/10 15:58:04 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,52 @@ char	*ft_input(void)
 	return (input);
 }
 
+void ft_display_ast(t_AST *node, int level)
+{
+    if (node == NULL)
+        return;
+
+    // Print indentation based on the level of the node in the tree
+    for (int i = 0; i < level; i++)
+        printf("  ");
+
+    // Check the type of the node and print relevant information
+    if (node->type == EXEC)
+    {
+        printf("Command: ");
+        if (node->argv)
+        {
+            for (int i = 0; node->argv[i]; i++)
+                printf("%s ", node->argv[i]);
+            printf("\n");
+        }
+        else
+        {
+            printf("(no arguments)\n");
+        }
+    }
+    else if (node->type == REDIR)
+    {
+        printf("Redirection: ");
+        if (node->mode == 0 & O_RDONLY == 0)
+            printf("Input redirection from file: %s\n", node->file);
+        else if (node->mode & O_WRONLY)
+        {
+            if (node->mode & O_TRUNC)
+                printf("Output redirection (truncate) to file: %s\n", node->file);
+            else
+                printf("Output redirection (append) to file: %s\n", node->file);
+        }
+        ft_display_ast(node->subcmd, level + 1);
+    }
+    else if (node->type == NPIPE)
+    {
+        printf("Pipe:\n");
+        ft_display_ast(node->left, level + 1); // Left side of the pipe
+        ft_display_ast(node->right, level + 1); // Right side of the pipe
+    }
+}
+
 void	ft_read_input(t_env **env)
 {
 	char			*input;
@@ -48,10 +94,14 @@ void	ft_read_input(t_env **env)
 			break ;
 		}
 		tokens = ft_create_list(input);
+		ast = ft_parsing(tokens);
+		printf("-----------------------\nAST :\n");
+		ft_display_ast(ast, 0);
 		//ast = parsecmd(tokens, *env);
 		free(input);
 	}
 }
+
 
 int	main(int ac, char **av, char **envp)
 {
