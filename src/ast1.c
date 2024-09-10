9125be55d421a6b88bc6f6d6379f9862ec20fdd2
Rafael Nascimento Cardoso <rafnasci@fu-r7-p1.s19.be>
2024-09-10 18:27:41 +0200

    ajout de la fonction ft_execution qui va exec une commande et qui verifie si elle est correcte

--- src/ast1.c
+++ src/ast1.c
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:57:28 by rafnasci          #+#    #+#             */
-/*   Updated: 2024/09/10 18:26:10 by rafnasci         ###   ########.fr       */
+/*   Updated: 2024/09/10 18:27:08 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,33 @@ int	ft_findpath(char **envp)
 		if (ft_strncmp("PATH", envp[i], 4) == 0)
 			return (i);
 	return (-1);
+}
+
+void	ft_execution(char **cmd, char **envp)
+{
+	char	*path_envp;
+	char	**all_paths;
+	int		i;
+	char	*path;
+
+	if (access(cmd[0], X_OK) == 0)
+		execve(cmd[0], cmd, envp);
+	path_envp = ft_substr(envp[ft_findpath(envp)], 5, 4654654);
+	all_paths = ft_split(path_envp, ":");
+	i = -1;
+	while (all_paths[++i])
+	{
+		path = ft_strjoin(ft_strjoin(all_paths[i], "/"), cmd[0]);
+		if (access(path, X_OK) == 0)
+			execve(path, cmd, envp);
+		free(path);
+	}
+	ft_putstr_fd("pipex: command not found: ", 2);
+	ft_putendl_fd(cmd[0], 2);
+	ft_free(cmd);
+	ft_free(all_paths);
+	free(path_envp);
+	exit(EXIT_FAILURE);
 }
 
 t_AST	*ft_execnode(void)
