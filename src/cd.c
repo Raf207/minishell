ef7fcfd581ae0395f8b58c9ce2a855fd9c2d333a
Mundir Cabrinety Ali <mucabrin@fu-r9-p8.s19.be>
2024-09-12 14:35:43 +0200

    only commit no push

--- src/cd.c
+++ src/cd.c
@@ -5,24 +5,29 @@ void	cd(t_token_list *token)
 	static const char	*oldpwd;
 	const char			*newpwd;
 	const char			*path;
+	int					er;
 
 	if (!oldpwd)
 		oldpwd = getcwd(NULL, 0);
 	path = token->next->value;
 	printf("oldpwd before chdir: %s\n", oldpwd);
 	if (!path || strncmp(path, "~", INT_MAX) == 0)
-	{
-		chdir(getenv("HOME"));
-		printf("path %s\n", path);
-	}
+		er = chdir(getenv("HOME"));
 	else if (strncmp(path, "-", INT_MAX) == 0)
 	{
 		path = oldpwd;
-		chdir(path);
+		er = chdir(path);
 	}
 	else
-		chdir(path);
-	// printf("path %s\n", path);
+		er = chdir(path);
+	if (er < 0)
+	{
+		printf("cd: %s: No such file or directory\n", path);
+		exit(1);
+	}
+	else 
+		exit(0);
+	 printf("path %s\n", path);
 	newpwd = getcwd(NULL, 0);
 	printf("newpwd after chdir: %s\n", newpwd);
 }
