0276cba07e71672e30d0d9592ee9ad84bd8e29af
Rafael Nascimento Cardoso <rafnasci@shi-r7-p3.s19.be>
2024-11-23 18:21:54 +0100

    insh

--- src/lexer/utils.c
+++ src/lexer/utils.c
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 14:49:38 by rafnasci          #+#    #+#             */
-/*   Updated: 2024/11/12 18:20:11 by rafnasci         ###   ########.fr       */
+/*   Updated: 2024/11/23 18:18:51 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,9 @@ void	ft_update_tok(t_token_list **token)
 			free(temp->value);
 			temp->value = ft_itoa(g_exitcode);
 		}
+		if (ft_strncmp("~", temp->value, 1) == 0)
+			temp->value = ft_strjoin(ft_strdup(getenv("HOME")),
+					&temp->value[1]);
 		temp = temp->next;
 	}
 }
